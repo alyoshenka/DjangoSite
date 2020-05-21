e974eede12c72b8775fa7754c6abeb8d6036fb68
alexi <alexibmost@gmail.com>
2020-05-21 13:12:46 -0700

    button array

--- leds/models.py
+++ leds/models.py
@@ -15,7 +15,43 @@ class Color(models.Model):
             + str(self.a) + ")"
 
 class LED(models.Model):
-    index = models.IntegerField()
+    index = models.IntegerField(default=0)
     color = models.ForeignKey(
         Color, on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return self.index
+
+    def color_style(self):
+        return "--color: " + self.color.label + ";"
+
+class Board(models.Model):
+    height = models.PositiveSmallIntegerField(default=8)
+    width = models.PositiveSmallIntegerField(default=32)
+    label = models.CharField(default="board", max_length=30)
+
+    def __init__(self):
+        super().__init__()
+        self.leds = []
+        for i in range(self.width * self.height):
+            self.leds.append(LED(index=i))
+
+    def size(self):
+        return self.width * self.height
+
+    def display_arr(self):
+        ret = []
+        for y in range(self.height):
+            row = []
+            for x in range(self.width):
+                idx = x * self.height + y
+                row.append(self.leds[idx])
+            ret.append(row)
+        return ret
+
+    def divisible_by_height(self, num):
+        return num % self.height == 0
+
+    def led(self, idx):
+        return self.leds[idx]
--- leds/views.py
+++ leds/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Color
+from .models import Color, Board
 
 
 def index(request):
-    return render(request, 'leds/Index.html', None)
+    board = Board()
+    arr = board.display_arr()
+    all_colors = Color.objects.all()
+    context = { 
+        'board': board,
+        'arr': arr,
+        'colors': all_colors,
+     }
+    return render(request, 'leds/index.html', context)
 
 def color(request, color_name):
     color = get_object_or_404(Color, label=color_name)
